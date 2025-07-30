7578a091d65fc602f6dfc5fc81eb4a094cadaaa9
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2025-07-29 21:23:57 -0400

    duplicates in an array + understanding

--- src/Arrays/Duplicate.java
+++ src/Arrays/Duplicate.java
@@ -26,5 +26,5 @@ This questions can be solved by brute force by simply using two pointers to trav
 But it requires o(n^2)
 
 the Optimal Solution is by using HashSet and simply check if the item presents in HashSet.
-It takes only O(1) time.
+It takes only O(n) time.
  */
