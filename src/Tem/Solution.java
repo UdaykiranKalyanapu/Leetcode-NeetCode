cb9aa7c7603b396a64b98c0f2386e0f0b9a355fe
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2025-09-10 23:06:06 -0400

    Temp Jumping Game

---
+++ src/Tem/Solution.java
@@ -0,0 +1,19 @@
+package Tem;
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > farthest) {
+                return false; // can't reach this index
+            }
+            farthest = Math.max(farthest, i + nums[i]);
+            if (farthest >= nums.length - 1) {
+                return true; // can reach or pass the last index
+            }
+        }
+
+        return true;
+    }
+}
