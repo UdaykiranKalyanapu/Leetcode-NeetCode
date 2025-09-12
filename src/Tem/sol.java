642b59945aba45bc4d1ca8f37d0451000a25366a
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2025-09-11 23:11:54 -0400

    Temp max AREA

---
+++ src/Tem/sol.java
@@ -0,0 +1,25 @@
+package Tem;
+
+public class sol {
+
+        public int maxArea(int[] height) {
+            int left = 0, right = height.length - 1;
+            int maxArea = 0;
+
+            while (left < right) {
+                int width = right - left;
+                int minHeight = Math.min(height[left], height[right]);
+                int area = width * minHeight;
+                maxArea = Math.max(maxArea, area);
+
+                // Move the pointer of the smaller line
+                if (height[left] < height[right]) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+
+            return maxArea;
+        }
+    }
