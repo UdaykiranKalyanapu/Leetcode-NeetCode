8aa59ca35160f378b87df491f90e59391d98b2e3
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2025-09-28 16:06:08 -0400

    Temp longestPerimerter

---
+++ src/Tem/longestPerimerter.java
@@ -0,0 +1,15 @@
+package Tem;
+import java.util.Arrays.*;
+
+public class longestPerimerter {
+
+        public int largestPerimeter(int[] nums) {
+            Arrays.sort(nums); // sort ascending
+            for (int i = nums.length - 1; i >= 2; i--) {
+                if (nums[i - 2] + nums[i - 1] > nums[i]) {
+                    return nums[i] + nums[i - 1] + nums[i - 2];
+                }
+            }
+            return 0;
+        }
+    }
