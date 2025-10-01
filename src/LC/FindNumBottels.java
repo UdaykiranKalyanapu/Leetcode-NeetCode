3ac6262a970c4004f26b9ca7b2d296e173d90332
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2025-10-01 15:20:24 -0400

    Number of Bottles

--- src/LC/FindNumBottels.java
+++ src/LC/FindNumBottels.java
@@ -1,28 +1,31 @@
 package LC;
 
+import static LC.FindNumBottels.numWaterBottles;
+
 public class FindNumBottels {
-    public static int numWaterBottles(int numBottles, int numExchange) {
-        // lets use simple if loop
-        int sum = 0;
 
+        public int static numWaterBottles(int numBottles, int numExchange) {
+            int sum = 0;
+            int empty = 0;
 
-        while(numBottles>0){
-            System.out.println(numBottles);
-            sum += numBottles;
-            int empty = numBottles;
-            if(empty>=numExchange){
-                numBottles = empty/numExchange;
+            while (numBottles > 0) {
+                // Drink all available bottles
+                sum += numBottles;
+                empty += numBottles;
+
+                // Exchange empty bottles
+                numBottles = empty / numExchange;
+                empty = empty % numExchange;
             }
-            else{
-                numBottles = 0;
-            }
 
+            return sum;
         }
-        return sum; }
+
+
 
     public static void main (String [] args){
         int numBottles = 9;
         int numExchange = 3;
-        System.out.println(numWaterBottles(numBottles,numExchange));
+        System.out.println(FindNumBottels.numWaterBottles(numBottles,numExchange));
     }
 }
