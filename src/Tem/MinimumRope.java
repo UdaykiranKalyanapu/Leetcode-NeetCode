7a4ffedadbb7373d5d1d06be35fd54ee9baf8eda
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2025-11-03 13:46:47 -0500

    MinimumRope

---
+++ src/Tem/MinimumRope.java
@@ -0,0 +1,22 @@
+package Tem;
+
+public class MinimumRope {
+        public int minCost(String colors, int[] neededTime) {
+            int totalCost = 0;
+            int maxTime = neededTime[0];
+
+            for (int i = 1; i < colors.length(); i++) {
+                if (colors.charAt(i) == colors.charAt(i - 1)) {
+                    // If same color, add the smaller removal cost to total
+                    totalCost += Math.min(maxTime, neededTime[i]);
+                    // Keep track of max removal time in the group (to keep it)
+                    maxTime = Math.max(maxTime, neededTime[i]);
+                } else {
+                    // Reset maxTime for the next group
+                    maxTime = neededTime[i];
+                }
+            }
+
+            return totalCost;
+        }
+    }
