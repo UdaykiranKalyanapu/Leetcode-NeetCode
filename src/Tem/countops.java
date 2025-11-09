85214e219be41b660b133db81ac38c174926047d
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2025-11-09 14:22:18 -0500

    count ops to get zero

---
+++ src/Tem/countops.java
@@ -0,0 +1,16 @@
+package Tem;
+
+public class countops{
+        public int countOperations(int num1, int num2) {
+            int count = 0;
+            while (num1 != 0 && num2 != 0) {
+                if (num1 >= num2) {
+                    num1 -= num2;
+                } else {
+                    num2 -= num1;
+                }
+                count++;
+            }
+            return count;
+        }
+    }
