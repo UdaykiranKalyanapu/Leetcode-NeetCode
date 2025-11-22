ec2909e20b7b3839735bd4178c1910c46210fe35
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2025-11-22 13:18:31 -0500

    Temp ops

---
+++ src/Tem/Ops.java
@@ -0,0 +1,14 @@
+package Tem;
+
+public class Ops {
+
+        public int minimumOperations(int[] nums) {
+            int operations = 0;
+            for (int num : nums) {
+                if (num % 3 != 0) {
+                    operations++;
+                }
+            }
+            return operations;
+        }
+    }
