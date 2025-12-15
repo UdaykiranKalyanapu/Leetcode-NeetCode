59e96e621f4d5f5af38b66f5b8596345f0b08769
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2025-12-15 15:04:59 -0500

    number of decent periods

---
+++ src/Tem/DecentwaysCount.java
@@ -0,0 +1,26 @@
+package Tem;
+
+public class DecentwaysCount {
+
+        public long getDescentPeriods(int[] prices) {
+            long ans = 0;
+            int cnt = 1;
+
+            for (int i = 0; i < prices.length; i++) {
+                if (i == 0) {
+                    ans += cnt;
+                    continue;
+                }
+
+                if (prices[i] == prices[i - 1] - 1) {
+                    cnt++;
+                } else {
+                    cnt = 1;
+                }
+
+                ans += cnt;
+            }
+
+            return ans;
+        }
+    }
