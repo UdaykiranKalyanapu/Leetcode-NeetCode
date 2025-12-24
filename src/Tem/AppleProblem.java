ca22c6f51e0a8831359215b19fc3e55dc1613077
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2025-12-23 21:16:45 -0500

    Apple Problems

---
+++ src/Tem/AppleProblem.java
@@ -0,0 +1,28 @@
+package Tem;
+import java.util.*;
+
+
+public class AppleProblem {
+
+        public int minimumBoxes(int[] apple, int[] capacity) {
+            int totalApples = 0;
+            for (int a : apple) {
+                totalApples += a;
+            }
+
+            Arrays.sort(capacity);
+
+            int usedBoxes = 0;
+            int currentCapacity = 0;
+
+            for (int i = capacity.length - 1; i >= 0; i--) {
+                currentCapacity += capacity[i];
+                usedBoxes++;
+                if (currentCapacity >= totalApples) {
+                    return usedBoxes;
+                }
+            }
+
+            return usedBoxes;
+        }
+    }
