5363638a85f15bef572ce02990aee395cb82f9f2
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2026-02-03 12:10:13 -0500

    Trionic Array

---
+++ src/Tem/tionicArray.java
@@ -0,0 +1,23 @@
+package Tem;
+
+public class tionicArray {
+
+
+        public boolean isTrionic(int[] nums) {
+            int n = nums.length;
+            int i = 1;
+            while (i < n && nums[i - 1] < nums[i]) {
+                i++;
+            }
+            int p = i - 1;
+            while (i < n && nums[i - 1] > nums[i]) {
+                i++;
+            }
+            int q = i - 1;
+            while (i < n && nums[i - 1] < nums[i]) {
+                i++;
+            }
+            int flag = i - 1;
+            return (p != 0) && (q != p) && (flag == n - 1 && flag != q);
+        }
+    }
