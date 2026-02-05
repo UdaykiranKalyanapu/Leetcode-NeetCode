51525c9618ff596cdfd12469bc01793fcb3dad8d
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2026-02-05 14:40:52 -0500

    Transform Array Easy

---
+++ src/Tem/CircularTransofrmedArray.java
@@ -0,0 +1,16 @@
+package Tem;
+
+public class CircularTransofrmedArray {
+
+        public int[] constructTransformedArray(int[] nums) {
+            int n = nums.length;
+            int[] result = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                int targetIdx = ((i + nums[i]) % n + n) % n;
+                result[i] = nums[targetIdx];
+            }
+
+            return result;
+        }
+    }
