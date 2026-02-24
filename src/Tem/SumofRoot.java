9dba50c6f592f216f1dd65219adab653acbc40ca
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2026-02-24 09:55:34 -0500

    Medium Sum of root

---
+++ src/Tem/SumofRoot.java
@@ -0,0 +1,25 @@
+package Tem;
+
+import javax.swing.tree.TreeNode;
+
+public class SumofRoot {
+
+
+        public int sumRootToLeaf(TreeNode root) {
+            return dfs(root, 0);
+        }
+
+        private int dfs(TreeNode node, int current) {
+            if (node == null) {
+                return 0;
+            }
+
+            current = current * 2 + node.val;
+
+            if (node.left == null && node.right == null) {
+                return current;
+            }
+
+            return dfs(node.left, current) + dfs(node.right, current);
+        }
+    }
