a4a7cee18627d3b9c4e65c677e1577409e51d4e7
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2026-03-06 18:19:18 -0500

    Minimum changes in Bits

---
+++ src/Tem/Miniops.java
@@ -0,0 +1,17 @@
+package Tem;
+
+public class Miniops {
+        public boolean checkOnesSegment(String s) {
+            boolean seenZero = false;
+
+            for(char c : s.toCharArray()) {
+                if(c == '0') {
+                    seenZero = true;
+                } else if(seenZero) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
