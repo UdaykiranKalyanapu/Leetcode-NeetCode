44355a82c13c0a09298f7a9bc813774965b2d432
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2026-05-03 01:12:10 -0400

    Easy Roate Strings

---
+++ src/Tem/RotateStrings.java
@@ -0,0 +1,11 @@
+package Tem;
+
+public class RotateStrings {
+
+        public boolean rotateString(String s, String goal) {
+            if (s.length() != goal.length())
+                return false;
+
+            return (s + s).contains(goal);
+        }
+    }
