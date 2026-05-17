768c4394cee4d7645bc07e9d2acf10448a1a57e5
udaykiran Kalyanapu <kalyanapuudaykiran32@gmail.com>
2026-05-16 23:32:14 -0400

    is good

---
+++ src/Tem/JumpGame3.java
@@ -0,0 +1,40 @@
+package Tem;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class JumpGame3 {
+
+        public boolean canReach(int[] arr, int start) {
+            int n = arr.length;
+
+            Queue<Integer> q = new LinkedList<>();
+            boolean[] vis = new boolean[n];
+
+            q.offer(start);
+            vis[start] = true;
+
+            while(!q.isEmpty()) {
+
+                int i = q.poll();
+
+                if(arr[i] == 0)
+                    return true;
+
+                int forward = i + arr[i];
+                int backward = i - arr[i];
+
+                if(forward < n && !vis[forward]) {
+                    vis[forward] = true;
+                    q.offer(forward);
+                }
+
+                if(backward >= 0 && !vis[backward]) {
+                    vis[backward] = true;
+                    q.offer(backward);
+                }
+            }
+
+            return false;
+        }
+    }
